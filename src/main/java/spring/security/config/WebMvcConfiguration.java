0aa46189036fedad3ec6adbf96eca7a2aa653d0c
chotire <chotire@gmail.com>
2016-04-04 10:12:12 +0900

    .

--- src/main/java/spring/security/config/WebMvcConfiguration.java
+++ src/main/java/spring/security/config/WebMvcConfiguration.java
@@ -9,5 +9,18 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("pages/index");
+        registry.addViewController("/index").setViewName("pages/index");
+        registry.addViewController("/login").setViewName("pages/login");
+        registry.addViewController("/blank").setViewName("pages/blank");
+        registry.addViewController("/buttons").setViewName("pages/buttons");
+        registry.addViewController("/flot").setViewName("pages/flot");
+        registry.addViewController("/forms").setViewName("pages/forms");
+        registry.addViewController("/grid").setViewName("pages/grid");
+        registry.addViewController("/icons").setViewName("pages/icons");
+        registry.addViewController("/morris").setViewName("pages/morris");
+        registry.addViewController("/notifications").setViewName("pages/notifications");
+        registry.addViewController("/panels-wells").setViewName("pages/panels-wells");
+        registry.addViewController("/tables").setViewName("pages/tables");
+        registry.addViewController("/typography").setViewName("pages/typography");
     }
 }
--- src/main/java/spring/security/config/WebSecurityConfiguration.java
+++ src/main/java/spring/security/config/WebSecurityConfiguration.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -18,9 +20,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
             .authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers("/bower_components/**", "/dist/**", "/js/**").permitAll()
+                .antMatchers("/bower_components/**", "/dist/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
+            .csrf().disable() // csrf를 enable하게되면 로그아웃을 반드시 post로 해야함.
             .formLogin()
                 .loginPage("/login").permitAll()
                 .and()
