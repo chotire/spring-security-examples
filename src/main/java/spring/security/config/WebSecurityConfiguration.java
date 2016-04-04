92c0a1341dc585bfc5ccb002d41c8b21901a30d5
chotire <chotire@gmail.com>
2016-04-04 18:02:29 +0900

    .

--- src/main/java/spring/security/config/WebSecurityConfiguration.java
+++ src/main/java/spring/security/config/WebSecurityConfiguration.java
@@ -6,21 +6,36 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    //@Autowired
+    //private DataSource dataSource;
+    
+    @Autowired
+    private UserDetailsService userDetailsService;
+    
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+        auth.userDetailsService(userDetailsService);
+
+        //auth.jdbcAuthentication().dataSource(dataSource);
+        
+        //test용 in-memory용 인증
+        //auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/index").permitAll()
+                .antMatchers("/flot", "/morris").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/tables").hasAnyRole("ADMIN")
                 .antMatchers("/bower_components/**", "/dist/**").permitAll()
+                .antMatchers("/h2/console/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .csrf().disable() // csrf를 enable하게되면 로그아웃을 반드시 post로 해야함.
@@ -28,5 +43,27 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .loginPage("/login").permitAll()
                 .and()
             .logout().permitAll();
+
+        
+        //default 설정
+//        http
+//            .authorizeRequests()
+//                .antMatchers("/").permitAll()
+//                .antMatchers("/bower_components/**", "/dist/**").permitAll()
+//                .antMatchers("/h2/console/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//            .csrf().disable() // csrf를 enable하게되면 로그아웃을 반드시 post로 해야함.
+//            .formLogin()
+//                .loginPage("/login").permitAll()
+//                .and()
+//            .logout().permitAll();
+
+        //h2 config...
+        http
+            .authorizeRequests()
+                .antMatchers("/h2/console/**").permitAll()
+                .and()
+            .headers().frameOptions().disable();
     }
 }
