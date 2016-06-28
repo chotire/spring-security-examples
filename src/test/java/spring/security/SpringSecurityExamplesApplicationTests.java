d3232b59fc180450f4f4768f871fa6a7871c1a76
choyongsang <chotire@gmail.com>
2016-06-28 11:39:52 +0900

    .

--- src/test/java/spring/security/SpringSecurityExamplesApplicationTests.java
+++ src/test/java/spring/security/SpringSecurityExamplesApplicationTests.java
@@ -1,40 +1,17 @@
 package spring.security;
 
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-
-import spring.security.domain.Account;
-import spring.security.domain.Authority;
-import spring.security.repository.AccountRepository;
-import spring.security.repository.MenuRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
 public class SpringSecurityExamplesApplicationTests {
-	@Autowired
-	private MenuRepository menuRepository;
-
-	@Autowired
-	private AccountRepository accountRepository;
-	
 	@Test
 	public void contextLoads() {
-		Account account = accountRepository.findByUsername("admin");
-		List<Authority> authorities = account.getAuthorities();
-		System.out.println("ss");
-//		Menu menu = menuRepository.findByUrl("/flot");
-//		List<Role> roles = menu.getRoles();
-//		for (Role role : roles) {
-//			String s = "%s, %s";
-//			System.out.println(String.format(s, role.getName(), role.getRemark()));
-//		}
-//		System.out.println(menu);
+
 	}
 }
